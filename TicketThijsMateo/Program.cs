36566233a9205ec32a630103e6e22e5a17f9a9eb
ThijsGeeraert2 <thijs.geeraert@student.vives.be>
2024-05-01 17:25:50 +0200

    added db creation prep

--- TicketThijsMateo/Program.cs
+++ TicketThijsMateo/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using TicketThijsMateo.Data;
+using TicketThijsMateo.Domains.Context;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,6 +14,11 @@ builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<TicketDBContext>(options =>
+{
+    options.UseSqlServer(connectionString); 
+});
 
 var app = builder.Build();
 
