4433a8102782e9346fb79569c221a16add776fa1
ThijsGeeraert2 <thijs.geeraert@student.vives.be>
2024-05-03 17:06:22 +0200

    localization

--- TicketThijsMateo/Controllers/HomeController.cs
+++ TicketThijsMateo/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Localization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Localization;
 using System.Diagnostics;
 using TicketThijsMateo.Models;
 
@@ -6,11 +8,12 @@ namespace TicketThijsMateo.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        private readonly IStringLocalizer<HomeController> _localizer;
+        public HomeController( IStringLocalizer<HomeController> localizer)
         {
-            _logger = logger;
+            _localizer = localizer;
+
         }
 
         public IActionResult Index()
@@ -27,6 +30,21 @@ namespace TicketThijsMateo.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [HttpPost]
+        public IActionResult SetAppLanguage(string lang, string returnUrl)
+        {
+            Response.Cookies.Append(
+                CookieRequestCultureProvider.DefaultCookieName,
+                CookieRequestCultureProvider.MakeCookieValue
+                (new RequestCulture(lang)),
+                new CookieOptions
+                {
+                    Expires = DateTimeOffset.UtcNow.AddYears(1)
+                });
+
+            return LocalRedirect(returnUrl);
         }
     }
 }
