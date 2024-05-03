2ac912399412ab77e8600abe01f34769906a83a7
ThijsGeeraert2 <thijs.geeraert@student.vives.be>
2024-05-03 10:40:43 +0200

    id db

---
+++ TicketThijsMateo/Controllers/TicketController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TicketThijsMateo.Controllers
+{
+    public class TicketController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
