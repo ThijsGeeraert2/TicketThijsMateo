476ed2a1bd675d777c1d2822a7f3e6f9c7068237
ThijsGeeraert2 <thijs.geeraert@student.vives.be>
2024-05-09 12:18:25 +0200

    check login

--- TicketThijsMateo/ViewModels/TicketCreateVM.cs
+++ TicketThijsMateo/ViewModels/TicketCreateVM.cs
@@ -4,8 +4,8 @@ namespace TicketThijsMateo.ViewModels
 {
     public class TicketCreateVM
     {
-        public string Naam { get; set; }
-        public string Voornaam { get; set; }
+        public string? Naam { get; set; }
+        public string? Voornaam { get; set; }
         public int wedstrijdId {  get; set; }
         public int Soortplaatsnr { get; set; }
         public IEnumerable<SelectListItem>? Soortplaatsen { get; set; }
