d49920240aa336d5f5646230b57a89dd1ee27bfe
ThijsGeeraert2 <thijs.geeraert@student.vives.be>
2024-05-17 01:25:11 +0200

    added delete shopping cart + bug fix

--- TicketThijsMateo/Controllers/ShoppingCartController.cs
+++ TicketThijsMateo/Controllers/ShoppingCartController.cs
@@ -1,17 +1,57 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using TicketThijsMateo.Domains.Context;
 using TicketThijsMateo.Extensions;
+using TicketThijsMateo.Services.Interfaces;
 using TicketThijsMateo.ViewModels;
 
 namespace TicketThijsMateo.Controllers
 {
     public class ShoppingCartController : Controller
     {
+
+       
         public IActionResult Index()
         {
             ShoppingCartVM? cartList = HttpContext.Session.GetObject<ShoppingCartVM>("ShoppingCart");
 
+            if (cartList == null)
+            {
+                cartList = new ShoppingCartVM();
+                cartList.Ticket = new List<TicketVM>();
+                HttpContext.Session.SetObject("ShoppingCart", cartList);
+            }
+
             return View(cartList);
         }
 
+        public IActionResult Delete(int? wedstrijdId)
+        {
+            if (wedstrijdId == null)
+            {
+                return BadRequest("Invalid item id.");
+            }
+
+            ShoppingCartVM? cartList = HttpContext.Session.GetObject<ShoppingCartVM>("ShoppingCart");
+
+            if (cartList == null)
+            {
+                return NotFound("Shopping cart not found.");
+            }
+
+            var itemToRemove = cartList.Ticket.FirstOrDefault(t => t.WedstrijdId == wedstrijdId);
+
+            if (itemToRemove != null)
+            {
+                cartList.Ticket.Remove(itemToRemove);
+                HttpContext.Session.SetObject("ShoppingCart", cartList);
+            }
+            else
+            {
+                return NotFound("Item not found in the shopping cart.");
+            }
+
+            return RedirectToAction("Index");
+        }
     }
 }
