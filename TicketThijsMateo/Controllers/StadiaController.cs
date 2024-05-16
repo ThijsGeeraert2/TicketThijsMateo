4bb0b6f97e9f0cfd59903389a97663a9a994740d
ThijsGeeraert2 <thijs.geeraert@student.vives.be>
2024-05-16 21:11:22 +0200

    stadia ophalen

---
+++ TicketThijsMateo/Controllers/StadiaController.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using TicketThijsMateo.Domains.Context;
+using TicketThijsMateo.Services;
+using TicketThijsMateo.Services.Interfaces;
+using TicketThijsMateo.ViewModels;
+
+namespace TicketThijsMateo.Controllers
+{
+    public class StadiaController : Controller
+    {
+
+
+        private IService<Stadium> stadiumService;
+
+
+        private readonly IMapper _mapper;
+
+        public StadiaController(IMapper mapper, IService<Stadium> stadiumService)
+        {
+            _mapper = mapper;
+            this.stadiumService = stadiumService;
+        }
+
+        public async Task<IActionResult> Index()  // add using System.Threading.Tasks;
+        {
+            var list = await stadiumService.GetAllAsync();
+            List<StadiumVM> listVM = _mapper.Map<List<StadiumVM>>(list);
+            return View(listVM);
+
+        }
+    }
+}
